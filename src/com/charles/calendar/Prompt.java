c0a341a3456638986c7808288ed0eb933dd296b7
mobd_권철진 <mobd_@DESKTOP-87VEMGK>
2021-09-06 13:47:43 +0900

    가상 달력 출력에 윤년 추가

--- src/com/charles/calendar/Prompt.java
+++ src/com/charles/calendar/Prompt.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT = "cal-> ";
 
 	public void runPrompt() {
 
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		int month = 1;
+		int year = 1;
 		while (true) {
+			System.out.println("년도를 입력하세요");
+			System.out.print("YEAR ->");			
+			year = scanner.nextInt();
+			
 			System.out.println("월을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print("MONTH ->");
 			month = scanner.nextInt();
 			if (month <= 0) {
 				System.out.printf("%d월은 존재하지 않습니다. 프로그램을 종료합니다.\n", month);
@@ -23,7 +27,7 @@ public class Prompt {
 				System.out.printf("%d월은 존재하지 않습니다. 다시 입력해주세요\n", month);
 				continue;
 			}
-			cal.printCalendar(2021, month);
+			cal.printCalendar(year, month);
 		}
 		System.out.println("Bye~");
 		scanner.close();
