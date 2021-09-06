d303d4864644b30599ff923c95c0f53225d8cedf
mobd_권철진 <mobd_@DESKTOP-87VEMGK>
2021-09-06 14:48:55 +0900

    첫째주 시작요일에 맞게 날짜 출력

--- src/com/charles/calendar/Calendar.java
+++ src/com/charles/calendar/Calendar.java
@@ -1,55 +1,53 @@
 package com.charles.calendar;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public boolean isLeapYear(int year) {
-		if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
 			return true;
-		}
-		else {			
+		} else {
 			return false;
 		}
 	}
-	
+
 	public int getMaxDaysOfMonth(int year, int month) {
-		if(isLeapYear(year)) {
+		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
-		}
-		else {			
+		} else {
 			return MAX_DAYS[month - 1];
 		}
 	}
-	
-	/**
-	 * - 월은 입력하면 해당월의 달력을 출력한다.
-	 * - 달력의 모양은 1단계에서 작성한 모양으로 만든다.
-	 * - 1일은 일요일로 정해도 무방하다.
-	 * - -1을 입력받기 전까지 반복을 이렵받는다.
-	 * - 프롬프트를 출력한다.
-	 */
-	
-	public void printCalendar(int year, int month) {
+
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("  <<%4d년%3d월>> \n", year, month);
 		System.out.println(" 일 월 화 수 목 금 토");
 		System.out.println("---------------------");
-		
+
 		int maxDay = getMaxDaysOfMonth(year, month);
-		
-		for(int i = 1; i <= maxDay; i++) {
+
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+		// print first line
+		int count = 7 - weekday;
+		int delimeter = (count >= 7) ? 0 : count;
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if(i % 7 == 0) {
+		}
+		System.out.println();
+		// print from second line to last
+		for (int i = count + 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delimeter) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 25 26 27 28 29");
+		System.out.println();
 	}
-	
-	
+
 }
--- src/com/charles/calendar/Prompt.java
+++ src/com/charles/calendar/Prompt.java
@@ -4,30 +4,62 @@ import java.util.Scanner;
 
 public class Prompt {
 
+	/**
+	 * @param week 요일명
+	 * @return 0 ~ 6 (0 = 일요일(default))
+	 */
+	public int getParseDay(String week) {
+
+		switch (week) {
+		case "일":
+			return 0;
+		case "월":
+			return 1;
+		case "화":
+			return 2;
+		case "수":
+			return 3;
+		case "목":
+			return 4;
+		case "금":
+			return 5;
+		case "토":
+			return 6;
+		default:
+			return 0;
+
+		}
+	}
 
 	public void runPrompt() {
 
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		int month = 1;
-		int year = 1;
+		int year = 2021;
+		int weekday = 0;
 		while (true) {
-			System.out.println("년도를 입력하세요");
-			System.out.print("YEAR ->");			
+			System.out.println("년도를 입력하세요.(exit: -1)");
+			System.out.print("YEAR ->");
 			year = scanner.nextInt();
-			
+			if (year == -1) {
+				break;
+			}
+
 			System.out.println("월을 입력하세요");
 			System.out.print("MONTH ->");
 			month = scanner.nextInt();
-			if (month <= 0) {
-				System.out.printf("%d월은 존재하지 않습니다. 프로그램을 종료합니다.\n", month);
-				break;
-			}
-			if (month > 12) {
+
+			System.out.println("첫번째 주의 요일을 입력하세요");
+			System.out.print("WEEKDAY ->");
+			String str_weekday = scanner.next();
+			weekday = getParseDay(str_weekday);
+
+			if (month > 12 || month < 1) {
 				System.out.printf("%d월은 존재하지 않습니다. 다시 입력해주세요\n", month);
 				continue;
 			}
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 		System.out.println("Bye~");
 		scanner.close();
