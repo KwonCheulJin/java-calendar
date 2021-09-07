8bd6cf1140985db1a756a3cc6ecb546956340174
mobd_권철진 <mobd_@DESKTOP-87VEMGK>
2021-09-07 10:37:38 +0900

    java-calendar 완강

--- src/com/charles/calendar/Calendar.java
+++ src/com/charles/calendar/Calendar.java
@@ -1,18 +1,41 @@
 package com.charles.calendar;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Calendar {
 
 	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String SAVE_FILE = "calendar.dat";
 	private HashMap<Date, PlanItem> planMap;
 	
 	
 	public Calendar() {
 		planMap = new HashMap<Date, PlanItem>();
+		File f = new File(SAVE_FILE);
+		if(!f.exists()) {
+			return;
+		}
+		try (Scanner scanner = new Scanner(f)){
+			while(scanner.hasNext()) {
+				String line = scanner.nextLine();
+				String[] words = line.split(",");
+				String date = words[0];
+				String detail = words[1];
+				PlanItem p = new PlanItem(date, detail);
+				planMap.put(p.getDate(), p);
+			}
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -23,6 +46,14 @@ public class Calendar {
 	public void registerPlan(String inputDate, String plan) {
 		PlanItem p = new PlanItem(inputDate, plan);
 		planMap.put(p.getDate(), p);
+		
+		File f = new File(SAVE_FILE);
+		String item = p.saveString();
+		try (FileWriter fw = new FileWriter(f, true)){
+			fw.write(item);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public PlanItem searchPlan(String inputDate){
--- src/com/charles/calendar/PlanItem.java
+++ src/com/charles/calendar/PlanItem.java
@@ -8,7 +8,7 @@ public class PlanItem {
 	public Date planDate;
 	public String detail;
 	public String peoples = "";
-	
+
 	public static Date getDateFromString(String inputDate) {
 		Date date = null;
 		try {
@@ -18,18 +18,24 @@ public class PlanItem {
 		}
 		return date;
 	}
-	
+
 	public PlanItem(String date, String detail) {
 		this.planDate = getDateFromString(date);
 		this.detail = detail;
 	}
-	
+
 	public Date getDate() {
 		return planDate;
 	}
-	
+
 	public void addPeople(String name) {
 		peoples += name + ",";
 	}
-	
+
+	public String saveString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String sdate = formatter.format(planDate);
+		return sdate + "," + detail + "\n";
+	}
+
 }
